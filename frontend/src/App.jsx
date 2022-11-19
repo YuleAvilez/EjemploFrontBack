b36d092be381fee8db0bfd5b830e21c00f66c14a
JholfrankRC <jholfrank202@gmail.com>
2022-11-19 12:50:26 -0500

    gut

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,43 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+// Context
+import { AuthProvider } from './context/AuthProvider';
+import { UsuariosProvider } from './context/UsuariosProvider'
 // Layout (LayoutAuth)
 import LayoutAuth from './Layout/LayoutAuth'
+import RutaProtegida from './Layout/RutaProtegida'
 // pages (usuarios)
 import Login from './pages/Login'
 import Registro from './pages/usuarios/Registro'
 import OlvidePassword from './pages/usuarios/OlvidePassword'
 import Confirmar from './pages/usuarios/Confirmar'
+// pages (usuarios protegidas)
+import Perfil from './pages/usuarios/Perfil'
+import CambiarPassword from './pages/usuarios/CambiarPassword.jsx'
+// pages (productos)
+
+
 function App() {
   return (
     <Router>
-      <Routes>
-        {/* RUTAS PUBLICAS */}
-        <Route path='/' element={<LayoutAuth />}>
-          <Route index element={<Login />} />
-          <Route path='registro' element={<Registro />} />
-          <Route path='olvide-password' element={<OlvidePassword />} />
-          <Route path='confirmar/:id' element={<Confirmar />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <UsuariosProvider>
+            <Routes>
+              {/* RUTAS PUBLICAS */}
+              <Route path='/' element={<LayoutAuth />}>
+                <Route index element={<Login />} />
+                <Route path='registro' element={<Registro />} />
+                <Route path='olvide-password' element={<OlvidePassword />} />
+                <Route path='confirmar/:id' element={<Confirmar />} />
+              </Route>
+              {/* Rutas Protegidas */}
+              <Route path='/perfil' element={<RutaProtegida />}>
+                <Route index element={<Perfil />} />
+                <Route path="cambiar-password" element={<CambiarPassword />} />
+              </Route>
+            </Routes>
+        </UsuariosProvider>
+      </AuthProvider>
     </Router>
   )
 }
